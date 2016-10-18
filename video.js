2784b32466df7f7a27f3ed49b67971697fc09451
Andrey Rubayko <tograilx@gmail.com>
2016-10-18 22:04:11 +0300

    changes

--- video.js
+++ video.js
@@ -1,14 +1,27 @@
+var app = angular.module('app', []);
+
 var webrtc = new SimpleWebRTC({
     // the id/element dom element that will hold "our" video
     localVideoEl: 'localVideo',
     // the id/element dom element that will hold remote videos
     remoteVideosEl: 'remotesVideos',
     // immediately ask for camera access
-    autoRequestMedia: true
+    autoRequestMedia: true,
+    debug: false,
+    detectSpeakingEvents: true,
+    autoAdjustMic: false
 });
 
-// we have to wait until it's ready
-webrtc.on('readyToCall', function () {
-    // you can name it anything
-    webrtc.joinRoom('123');
-});
+app.controller('video', [ '$scope', function ($scope) {
+    $scope.createRoom = function () {
+        webrtc.createRoom('andrew', function (err, name) {
+            webrtc.joinRoom('andrew');
+        });
+    }
+
+    $scope.joinRoom = function () {
+        webrtc.joinRoom('andrew');
+    }
+}]);
+
+
